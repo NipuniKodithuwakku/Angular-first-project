7c1725acaa58b4d14fa2cb19cf154a98273fa66c
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-06-15 10:11:08 +0530

    update the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,8 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  // styleUrls: ["./app.component.css"],
-  styles: [
-    `
-      h1 {
-        color: red;
-      }
-    `,
-  ],
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  name = "Nipuni";
+  country = "Sri Lanka";
 }
